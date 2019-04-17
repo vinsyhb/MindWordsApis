34959be58848d21d4ff60ca15f43bfa66cd46bd4
vhb0007 <vinaykumar.basavaraju@walmartlabs.com>
2019-04-17 21:09:46 +0530

    allowing cors for all the methods

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
+var cors = require('cors')
 
+server.use(cors());
 server.use(middlewares);
 server.use(router);
-server.use( ( req, res, next ) => {
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    next();
-  } );
+// server.use( ( req, res, next ) => {
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     next();
+//   } );
 server.listen(port);
