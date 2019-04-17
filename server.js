6929131fa1ff8c7b3098dd3cdca630d874f93820
vhb0007 <vinaykumar.basavaraju@walmartlabs.com>
2019-04-17 18:29:36 +0530

    allowing cors for all the methods

--- server.js
+++ server.js
@@ -6,5 +6,8 @@ const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
-
+server.use( ( req, res, next ) => {
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    next();
+  } );
 server.listen(port);
