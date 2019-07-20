ffef65493ebc69bac2a15e8325afa8c8302517f6
vinsyhb <vinay1n1@gmail.com>
2019-07-20 12:07:20 +0530

    a

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 var cors = require('cors')
+server.use( ( req, res, next ) => {
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST,HEAD, OPTIONS,PUT, DELETE");
+    next();
+  } );
 
 server.use(cors());
 server.use(middlewares);
 server.use(router);
-// server.use( ( req, res, next ) => {
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//     next();
-//   } );
+
 server.listen(port);
